2bd0ea374e2554f53db6e09b88abef9769744e8d
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-10 22:40:58 -0500

    Adding junit @Autowired

--- TDDPrep-moto-jdbc/src/test/java/com/interviewPrep/TDDPrepmotojdbc/dao/MotrocycleDaoTest.java
+++ TDDPrep-moto-jdbc/src/test/java/com/interviewPrep/TDDPrepmotojdbc/dao/MotrocycleDaoTest.java
@@ -3,12 +3,18 @@ package com.interviewPrep.TDDPrepmotojdbc.dao;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class MotrocycleDaoTest {
+
+    @Autowired
+    protected MotrocycleDao dao;
 
     @Before
     public void setUp() throws Exception {
