b54d5ea146f4135106c89e8b5745b9d13363f682
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-10 20:14:11 -0500

    Create Getters and Setters

--- TDDPrep-moto-jdbc/src/main/java/com/interviewPrep/TDDPrepmotojdbc/Motorcycle.java
+++ TDDPrep-moto-jdbc/src/main/java/com/interviewPrep/TDDPrepmotojdbc/Motorcycle.java
@@ -8,5 +8,51 @@ public class Motorcycle {
     private String year;
     private String color;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
