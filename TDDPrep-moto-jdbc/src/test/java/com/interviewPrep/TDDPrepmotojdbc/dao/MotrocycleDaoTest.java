6354579cf50fae0e7441218039ffdd811fffe919
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-10 22:07:59 -0500

    Adding junit @RunWith annotation

--- TDDPrep-moto-jdbc/src/test/java/com/interviewPrep/TDDPrepmotojdbc/dao/MotrocycleDaoTest.java
+++ TDDPrep-moto-jdbc/src/test/java/com/interviewPrep/TDDPrepmotojdbc/dao/MotrocycleDaoTest.java
@@ -2,9 +2,12 @@ package com.interviewPrep.TDDPrepmotojdbc.dao;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 public class MotrocycleDaoTest {
 
     @Before
