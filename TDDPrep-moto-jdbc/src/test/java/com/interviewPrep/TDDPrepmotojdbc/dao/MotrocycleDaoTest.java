246276e1e919bf16df1f70e8046d1e617c2b13a5
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-10 22:04:35 -0500

    Adding junit test for DAO

---
+++ TDDPrep-moto-jdbc/src/test/java/com/interviewPrep/TDDPrepmotojdbc/dao/MotrocycleDaoTest.java
@@ -0,0 +1,17 @@
+package com.interviewPrep.TDDPrepmotojdbc.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MotrocycleDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getMotorcycle() {
+    }
+}
