66d572912ff6d27f77bd10d30a55a2cb64f08c73
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-10 19:59:42 -0500

    Adding dependancies

---
+++ TDDPrep-moto-jdbc/src/main/java/com/interviewPrep/TDDPrepmotojdbc/TddPrepMotoJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.interviewPrep.TDDPrepmotojdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddPrepMotoJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddPrepMotoJdbcApplication.class, args);
+	}
+
+}
---
+++ TDDPrep-moto-jdbc/src/test/java/com/interviewPrep/TDDPrepmotojdbc/TddPrepMotoJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewPrep.TDDPrepmotojdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPrepMotoJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
