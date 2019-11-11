473bac96f22ca5926666e5f2196ad194312da0b8
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-10 20:13:46 -0500

    Create Properties

--- TDDPrep-moto-jdbc/src/main/java/com/interviewPrep/TDDPrepmotojdbc/Motorcycle.java
+++ TDDPrep-moto-jdbc/src/main/java/com/interviewPrep/TDDPrepmotojdbc/Motorcycle.java
@@ -1,4 +1,12 @@
 package com.interviewPrep.TDDPrepmotojdbc;
 
 public class Motorcycle {
+    private int id;
+    private String vin;
+    private String make;
+    private String model;
+    private String year;
+    private String color;
+
+
 }
