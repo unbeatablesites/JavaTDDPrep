876532077749de4b898525ae0a94ba2b999ae580
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-10 20:43:26 -0500

    Getting one motorcycle for the given id

--- TDDPrep-moto-jdbc/src/main/java/com/interviewPrep/TDDPrepmotojdbc/dao/MotrocycleDao.java
+++ TDDPrep-moto-jdbc/src/main/java/com/interviewPrep/TDDPrepmotojdbc/dao/MotrocycleDao.java
@@ -1,4 +1,13 @@
 package com.interviewPrep.TDDPrepmotojdbc.dao;
 
+import com.interviewPrep.TDDPrepmotojdbc.dto.Motorcycle;
+
 public interface MotrocycleDao {
+    /**
+     * Getting one motorcycle for the given id
+     * @param id
+     * @return Motorcycle
+     */
+    Motorcycle getMotorcycloe(int id);
+
 }
